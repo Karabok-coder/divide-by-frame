6adf420c03f2ba08af17d4110a534e5c0ebc45fa
Karabok <71427586+Karabok-coder@users.noreply.github.com>
2022-06-18 00:46:17 +0300

    Add files via upload

---
+++ divide by frame/main.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import cv2
+from sys import argv
+from progress.bar import Bar
+
+script, src, from_frame = argv
+number_frame = 0
+is_true = False
+
+cap = cv2.VideoCapture(src)
+length_video = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+bar = Bar('Process', max = length_video - int(from_frame))
+
+while True:
+    _, frame = cap.read()
+
+    if int(from_frame) == number_frame: is_true = True
+
+    number_frame += 1
+
+    if is_true:
+        im = Image.fromarray(frame)
+        im.save('img/' + str(number_frame) + '.png')
+        bar.next()
+
+    length_video -= 1
+    if length_video < 1: break
+
+bar.finish()
